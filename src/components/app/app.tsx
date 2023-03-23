f26a5fbce5041cc38d660fa624155a2f5b71a830
Garri-99 <103282736+Garri-99@users.noreply.github.com>
2023-03-23 16:28:23 +0400

    deploy

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import { FibonacciPage } from "../fibonacci-page/fibonacci-page";
 import { ListPage } from "../list-page/list-page";
 import { MainPage } from "../main-page/main-page";
@@ -12,7 +11,7 @@ import styles from './app.module.css'
 function App() {
   return (
     <div className={styles.app}>
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route path="/" exact>
             <MainPage />
@@ -36,7 +35,7 @@ function App() {
             <ListPage />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
